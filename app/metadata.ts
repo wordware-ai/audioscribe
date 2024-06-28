fd5c7c739208dc92b081ffa6f0e5a721e5ad048a
Kamil <zowczak.kamil@gmail.com>
2024-06-28 13:16:37 +0800

    Metadata & OG image

---
+++ app/metadata.ts
@@ -0,0 +1,15 @@
+const siteMetadata = {
+  title: 'Audioscribe · AI-powered Record-to-Text tool built by Wordware',
+  author: 'Wordware',
+  headerTitle: 'Audioscribe',
+  description:
+    'Audioscribe is an AI-powered tool for turning audio recordings into text. It uses cutting-edge AI to transcribe audio and help you with your work.',
+  language: 'en-us',
+  theme: 'light', // system, dark or light
+  siteUrl: 'https://audioscribe.wordware.ai', // your website URL
+  socialBanner: '/social/og.png', // add social banner in the public folder
+  email: 'kamil@copycopter.ai',
+  locale: 'en-US',
+}
+
+export default siteMetadata
