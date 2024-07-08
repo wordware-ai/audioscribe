f21fb5db479543c1a104d7a9a072728ae016e1b0
Kamil <zowczak.kamil@gmail.com>
2024-07-08 12:51:57 +0800

    ✨ (PHButton.tsx): Update PHButton component to display a Product Hunt badge with a link to the Audioscribe project page on Product Hunt. Remove unused Button component and add new content for launching the open-source code with a countdown if available.

--- app/_components/PHButton.tsx
+++ app/_components/PHButton.tsx
@@ -34,21 +34,37 @@ const PHButton = () => {
   }, [])
 
   return (
-    <Button
-      size={'lg'}
-      variant={'secondary'}
-      className="absolute right-2 top-2 bg-[#ff6154] text-white hover:bg-[#ff6154]"
-      asChild>
+    <div className="flex-center absolute right-2 top-2 flex-row gap-1 md:flex-col">
       <a
-        href="https://github.com/wordware-ai/audioscribe/"
-        target="_blank"
-        className="flex-center gap-2 text-center text-sm font-light">
-        <Sparkle size={14} />
-        Open Source launching
-        <br />
-        on Product Hunt {countdown && `in ${countdown}`}
+        href="https://www.producthunt.com/posts/audioscribe-ai-powered-record-to-text?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-audioscribe&#0045;ai&#0045;powered&#0045;record&#0045;to&#0045;text"
+        target="_blank">
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=469224&theme=light"
+          alt="Audioscribe&#0032;&#0183;&#0032;AI&#0045;powered&#0032;Record&#0045;to&#0045;Text - open&#0045;source&#0032;AI&#0032;agent&#0032;built&#0032;on&#0032;Wordware | Product Hunt"
+          style={{ width: '167px', height: '36px' }}
+          width="167"
+          height="36"
+        />
       </a>
-    </Button>
+      <div className="text-center text-sm text-[#ff6154]">
+        Launching Open Source code
+        <br />
+        {countdown && `in ${countdown}`}
+      </div>
+    </div>
+    // <Button
+    //   size={'lg'}
+    //   variant={'secondary'}
+    //   className="absolute right-2 top-2 bg-[#ff6154] text-white hover:bg-[#ff6154]"
+    //   asChild>
+    //   <a
+    //     href="https://github.com/wordware-ai/audioscribe/"
+    //     target="_blank"
+    //     className="flex-center gap-2 text-center text-sm font-light">
+
+    //   </a>
+
+    // </Button>
   )
 }
 
